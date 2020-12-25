3479ab2017f641d9514ea1aaf44ac4a08c7793da
Sujith1219 <68234378+SujithGunasekaran@users.noreply.github.com>
2020-12-25 20:17:27 +0530

    Api key removed

--- client/src/Api/YoutubeAPIKey.js
+++ client/src/Api/YoutubeAPIKey.js
@@ -1,3 +1,1 @@
-export const YoutubeAPIKey = 'AIzaSyCX8tLpg7fVoyhRoQui5Elh-qSg-iiRp8A';
-
-// export const YoutubeAPIKey = 'AIzaSyBIK4DmqA4B0YlGntpUyxqZdnTrH8xz1g8';
+export const YoutubeAPIKey = '<<< Your API KEY >>>';
